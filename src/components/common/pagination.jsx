18742c650dd6c7929bd0e1f116acbeef57efa395
Christine Maynard <christinekrm02@gmail.com>
2021-01-15 21:25:57 -0500

    displayed pages. will handle page changes

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -1,15 +1,25 @@
 import React from "react";
+import _ from "lodash";
 
 const Pagination = props => {
+  const { itemsCount, pageSize } = props;
+
+  /* raise pagesCount to nearest whole value because floating points still return a page */
+
+  const pagesCount = Math.ceil(itemsCount / pageSize);
+  if (pagesCount === 1) return null;
+  const pages = _.range(1, pagesCount + 1);
   return (
     <nav>
       <ul className="pagination">
-        <li className="page-items">
-          {/* eslint-disable-next-line jsx-a11y/anchor-has-content*/}
-          <a href="#!" className="page-link">
-            1
-          </a>
-        </li>
+        {pages.map(page => (
+          <li key={page} className="page-items">
+            {/* eslint-disable-next-line jsx-a11y/anchor-has-content*/}
+            <a href="#!" className="page-link">
+              {page}
+            </a>
+          </li>
+        ))}
       </ul>
     </nav>
   );
