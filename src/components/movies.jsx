c414b8fd02251d9b6638cb2321cc25f1d3fb7067
Christine Maynard <christinekrm02@gmail.com>
2021-01-15 21:07:15 -0500

    Added Pagination component

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import Like from "./common/like";
+import Pagination from "./common/pagination";
 import { getMovies } from "../services/fakeMovieService";
 
 class Movies extends Component {
   state = {
     movies: getMovies(),
+    pageSize: 4,
   };
   handleDelete = movie => {
     const movies = this.state.movies.filter(m => m._id !== movie._id);
@@ -22,6 +24,9 @@ class Movies extends Component {
     this.setState({
       movies,
     });
+  };
+  handlePageChange = page => {
+    console.log("handlePageChange:", page);
   };
   render() {
     const { length: count } = this.state.movies;
@@ -64,6 +69,11 @@ class Movies extends Component {
             ))}
           </tbody>
         </table>
+        <Pagination
+          itemsCount={count}
+          pageSize={this.state.pageSize}
+          onPageChange={this.handlePageChange}
+        />
       </React.Fragment>
     );
   }
