8f3309144671081e6c158c47a36ce1afa16cf068
Christine Maynard <christinekrm02@gmail.com>
2021-01-15 21:14:19 -0500

    rendered pagination to page. will make dynamic

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Pagination = props => {
-  return null;
+  return (
+    <nav>
+      <ul className="pagination">
+        <li className="page-items">
+          {/* eslint-disable-next-line jsx-a11y/anchor-has-content*/}
+          <a href="#!" className="page-link">
+            1
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Pagination;
