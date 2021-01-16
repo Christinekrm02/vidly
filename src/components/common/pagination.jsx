059d627d9253f9c6c14cafc44fcfcbf8218d2437
Christine Maynard <christinekrm02@gmail.com>
2021-01-15 21:42:05 -0500

    Added page handle changes. will add paginating data'

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import _ from "lodash";
 
 const Pagination = props => {
-  const { itemsCount, pageSize } = props;
+  const { itemsCount, pageSize, currentPage, onPageChange } = props;
+  /* console.log(currentPage); */
 
   /* raise pagesCount to nearest whole value because floating points still return a page */
-
   const pagesCount = Math.ceil(itemsCount / pageSize);
   if (pagesCount === 1) return null;
   const pages = _.range(1, pagesCount + 1);
@@ -13,9 +13,15 @@ const Pagination = props => {
     <nav>
       <ul className="pagination">
         {pages.map(page => (
-          <li key={page} className="page-items">
+          <li
+            key={page}
+            /* highlights each page per active class */
+            className={page === currentPage ? "page-item active" : "page-item"}>
             {/* eslint-disable-next-line jsx-a11y/anchor-has-content*/}
-            <a href="#!" className="page-link">
+            <a
+              href="#!"
+              className="page-link"
+              onClick={() => onPageChange(page)}>
               {page}
             </a>
           </li>
--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -7,6 +7,7 @@ class Movies extends Component {
   state = {
     movies: getMovies(),
     pageSize: 4,
+    currentPage: 1,
   };
   handleDelete = movie => {
     const movies = this.state.movies.filter(m => m._id !== movie._id);
@@ -26,10 +27,14 @@ class Movies extends Component {
     });
   };
   handlePageChange = page => {
-    console.log("handlePageChange:", page);
+    this.setState({
+      currentPage: page,
+    });
+    /* console.log("handlePageChange:", page); */
   };
   render() {
     const { length: count } = this.state.movies;
+    const { pageSize, currentPage } = this.state;
     if (count === 0) return <p>No movies left in database</p>;
     return (
       <React.Fragment>
@@ -71,7 +76,8 @@ class Movies extends Component {
         </table>
         <Pagination
           itemsCount={count}
-          pageSize={this.state.pageSize}
+          pageSize={pageSize}
+          currentPage={currentPage}
           onPageChange={this.handlePageChange}
         />
       </React.Fragment>
